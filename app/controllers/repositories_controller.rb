f5acb930fb04e5914908a3f2d8114236d66b309f
burakkocak884 <45079524+burakkocak884@users.noreply.github.com>
2019-06-06 11:57:18 -0400

    Update repositories_controller.rb

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,8 +6,8 @@ class RepositoriesController < ApplicationController
 
 		  def github_search
 
-				client_id = 'Iv1.bf285af96e5f4b3d'	
-				client_secret = 'adf3dd4d4d1c84efe02bffd12017b6b4ed4fd750'
+				client_id = ''	
+				client_secret = ''
 				url = 'https://api.github.com/search/repositories'
 		  resp = Faraday.get(url) do |req|
           req.params = {q: "#{params[:query]}"}
